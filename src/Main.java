4989a9941187468da8710b752853396dfc30c0e6
yura178500 <yura178500@yandex.ru>
2022-10-20 14:52:07 +0300

    Задача с массивами Исправил

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
+import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        int[] weigts = {90,91,93,92,85,87,42,0,87};
-        weigts[0] = 90;
-        int jeniwerBeht = weigts[0];
-        System.out.println(jeniwerBeht);
-        System.out.println(weigts[0]);
-        System.out.println(weigts[13]);
-        int jeniwerBeht = 0;
-        System.out.println(weigts[jeniwerBeht]);
-        for (int i = 0; i < weigts.length; i++ ){
-        System.out.println(weigts[i]);
-
+    int[] day = generateRandomArray();
+    public static int[] generateRandomArray() {
+        //Задача 1
+        java.util.Random random = new java.util.Random();
+        int[] day = new int[30];
+        for (int i = 0; i < day.length; i++) {
+            int[] amount = new int[30];
+            for (int j = 0; j < amount.length; j++) {
+                amount[j] = random.nextInt(100_000) + 100_000;
+                amount += day[amount[j]];
+                System.out.println("Сумма трат за месяц составила" + amount);
+            }
         }
-    }}
+
+        // Задача 2
+        java.util.Random random = new java.util.Random();
+        int[] day = new int[30];
+        int[] amount = new int[30];
+        for (int i = 0; i < day.length; i++) {
+            System.out.println(day[i]);
+            int maxAmount = -1;
+            for (int j = 0; j < amount.length; j++) {
+                if (amount[i] > maxAmount) {
+                    maxAmount = amount[i];
+                    System.out.println("Максимальная сумма трат за день составила" + maxAmount);}
+            }       }
+        int minAmount = -1;
+        for (int j = 0; j < amount.length; j++) {
+            if (amount[j] > minAmount) {
+                minAmount = amount[j];
+                System.out.println("Мигимальная сумма трат за день составила" + minAmount);
+            }
+        }
+
+        //Зажача 3
+        int[] day = new int[30];
+        for (int i = 0; i < day.length; i++) {
+            int[] amount = new int[30];
+            for (int i = 0; i < amount.length; i++) {
+                amount[i] = random.nextInt(100_000) + 100_000;
+                amount += day[amount[i]];
+                amount = amount / 30;
+                System.out.println("Средняя cумма трат за месяц составила" + amount);}
+        }
+    }
+    // Задача 4
+    char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length; i++) {
+        Arrays.sort(reverseFullName);
+        System.out.println(Arrays.toString(reverseFullName));}}}
+
+
+
+
+
+
+
+
+
